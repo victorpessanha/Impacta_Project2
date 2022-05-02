4fa7615fabc9f5e15f61bde55714903c9800fa3d
victor <victor.pessanha@gmail.com>
2022-05-02 11:16:45 -0300

    Cap3 La2 - Verifica se par ou impar.

---
+++ src/cap03lab01/ParImparLab2.java
@@ -0,0 +1,22 @@
+package cap03lab01;
+
+import java.util.Scanner;
+
+public class ParImparLab2 {
+
+	public static void main(String[] args) {
+		int numero;
+		
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Type a number to verify: ");
+		numero = scan.nextInt();
+		
+		if(numero % 2 == 0) {
+			System.out.println("That's a even number");
+		} else {
+			System.out.println("That's a odd number");
+		}
+
+	}
+
+}
